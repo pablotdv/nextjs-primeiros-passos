5260bda68a4b022934166aee17be0d1b5415a677
Pablo T. de Vargas <pablotdvsm@gmail.com>
2023-09-14 17:10:35 -0700

    Nextjs primeiros passos

---
+++ app/contatos/page.js
@@ -0,0 +1,40 @@
+import styles from './contatos.module.css'
+
+async function buscarContatos() {
+  const resposta = await fetch("http://localhost:3000/api/contatos");
+  return await resposta.json();
+}
+
+export default async function Page() {
+  const contatos = await buscarContatos();
+  return (
+    <>
+      <h1>Olá NextJS - Contatos Page</h1>
+      <table className={styles.contatos}>
+        <thead>
+          <tr>
+            <th>Nome</th>
+            <th>Endereço</th>
+            <th>Telefone</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            contatos.map((contato) =>
+              <tr>
+                <td>{contato.nome}</td>
+                <td>{contato.endereco}</td>
+                <td>{contato.telefone}</td>
+              </tr>
+            )
+          }
+        </tbody>
+        <tfoot>
+          <tr>
+            <td colSpan="3">Total contatos: {contatos.length}</td>
+          </tr>
+        </tfoot>
+      </table>
+    </>
+  )
+}
---
+++ app/sobre/page.js
@@ -0,0 +1,8 @@
+export default function Page() {
+    return (
+        <>
+            <h1>Olá NextJS - Página Sobre</h1>
+            <h2>Ok</h2>
+        </>
+    )
+}
